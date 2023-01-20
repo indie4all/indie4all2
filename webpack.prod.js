2a53c0c278a865850a96bf6f5577c7d6769f8f9f
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-01-20 10:00:46 +0100

    Keep API methods in production mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = common.map(config => merge(config, { mode: 'production' }))
+module.exports = common.map(config => merge(config, { 
+    mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: { 
+                keep_fnames: true
+            }
+        })]
+    }
+ }))

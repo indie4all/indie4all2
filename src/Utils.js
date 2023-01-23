c658cbe28f70f4ae9c4b48243fea1e1cc5b3dbd3
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-01-23 14:58:38 +0100

    [Utils] Refactorization and removed unused methods

--- src/Utils.js
+++ src/Utils.js
@@ -26,20 +26,14 @@ export default class Utils {
     }
 
     static toJSON(form) {
+
         var obj = {};
         var elements = form.querySelectorAll("input, select, textarea");
-        for (var i = 0; i < elements.length; ++i) {
-            var element = elements[i];
-            var name = element.name;
-            var value = element.value;
-            var type = element.type;
-
+        elements.forEach(element => {
+            const {name, value, type} = element;
             let matchedArray = name.match(/^([^[]*)\[(\d+)\](?:\[([^\]]+)\])?$/);
-            if (matchedArray)
-            {
-                let field = matchedArray[1];
-                let position = matchedArray[2];
-                let subField = matchedArray.length > 3 ? matchedArray[3] : null;
+            if (matchedArray) {
+                const [, field, position, subField] = matchedArray;
                 if (!obj[field]) obj[field] = [];
                 if (subField) {
                     if (!obj[field][position]) obj[field][position] = {};
@@ -53,7 +47,7 @@ export default class Utils {
                 if (name && (type != 'radio' || element.checked))
                     obj[name] = value;
             }
-        }
+        });
         return obj;
     }
 
@@ -94,11 +88,7 @@ export default class Utils {
     }
 
     static parseBoolean = function (string) {
-        switch (string.toLowerCase()) {
-            case "true": return true;
-            case "false": return false;
-            default: return void 0;
-        }
+        return string.toLowerCase() === "true";
     };
 
     static booleanToString(bool) {
@@ -124,16 +114,9 @@ export default class Utils {
     }
 
     static swap(elementOrigin, elementTarget) {
-        var div1 = $(elementOrigin);
-        var div2 = $(elementTarget);
-
-        var tdiv1 = div1.clone();
-        var tdiv2 = div2.clone();
-
-        if (!div2.is(':empty')) {
-            div1.replaceWith(tdiv2);
-            div2.replaceWith(tdiv1);
-        }
+        const $target = $(elementTarget);
+        const $replaced = $(elementOrigin).replaceWith($target.clone());
+        $target.replaceWith($replaced);
     }
 
     static isURL(st) {
@@ -152,14 +135,7 @@ export default class Utils {
     static isUrlWithinDomains(url, allowedDomains) {
         if (!this.isURL(url))
             return false;
-
-        for (var i = 0; i < allowedDomains.length; i++) {
-            var allowedDomain = allowedDomains[i];
-            if (url.startsWith(allowedDomain))
-                return true;
-        }
-
-        return false;
+        return allowedDomains.some(dom => url.startsWith(dom));
     }
 
     static isIndieResource(url) {
@@ -171,86 +147,12 @@ export default class Utils {
         return typeof data === 'string' && data.match(pattern);
     }
 
-    static isOnlyOneWord(string) {
-        if (!string) return false;
-        string = string.trim();
-        var splited = string.split(" ");
-        return (splited.length == 1);
-    }
-
-    static getNumber(number) {
-        return number;
-    }
-
     static hasNameInParams(widgetInstance) {
         return (widgetInstance.params && (widgetInstance.params.name && (widgetInstance.params.name.length > 0)));
     }
 
     static isInteractiveVideo(url) {
         return this.isUrlWithinDomains(url, ["https://indieopen.upct.es", "https://backendcpcd-servicio-gateway.azuremicroservices.io", "https://scgateway-cpcd-upct-gufcfdgzgee5fydr.z01.azurefd.net"]);
-    }
-
-    static getAllUrlParams(url) {
-        var queryString = url.split('?')[1];
-
-        // we'll store the parameters here
-        var obj = {};
-
-        // if query string exists
-        if (queryString) {
-
-            // stuff after # is not part of query string, so get rid of it
-            queryString = queryString.split('#')[0];
-
-            // split our query string into its component parts
-            var arr = queryString.split('&');
-
-            for (var i = 0; i < arr.length; i++) {
-                // separate the keys and the values
-                var a = arr[i].split('=');
-
-                // set parameter name and value (use 'true' if empty)
-                var paramName = a[0];
-                var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-                // (optional) keep case consistent
-                paramName = paramName.toLowerCase();
-                if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-
-                // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-                if (paramName.match(/\[(\d+)?\]$/)) {
-
-                    // create key if it doesn't exist
-                    var key = paramName.replace(/\[(\d+)?\]/, '');
-                    if (!obj[key]) obj[key] = [];
-
-                    // if it's an indexed array e.g. colors[2]
-                    if (paramName.match(/\[\d+\]$/)) {
-                        // get the index value and add the entry at the appropriate position
-                        var index = /\[(\d+)\]/.exec(paramName)[1];
-                        obj[key][index] = paramValue;
-                    } else {
-                        // otherwise add the value to the end of the array
-                        obj[key].push(paramValue);
-                    }
-                } else {
-                    // we're dealing with a string
-                    if (!obj[paramName]) {
-                        // if it doesn't exist, create property
-                        obj[paramName] = paramValue;
-                    } else if (obj[paramName] && typeof obj[paramName] === 'string') {
-                        // if property does exist and it's a string, convert it to an array
-                        obj[paramName] = [obj[paramName]];
-                        obj[paramName].push(paramValue);
-                    } else {
-                        // otherwise add the property
-                        obj[paramName].push(paramValue);
-                    }
-                }
-            }
-        }
-
-        return obj;
     }
 
     static encodeBlobAsBase64DataURL(blob) {
@@ -286,6 +188,3 @@ export default class Utils {
     }
 
 }
-
-
-

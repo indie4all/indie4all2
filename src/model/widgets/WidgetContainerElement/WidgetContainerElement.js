c425b94781456d5aa1c009e8133bb981e9005ab1
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-02-02 11:49:13 +0100

    [Fix] Do not allow copying items from containers

--- src/model/widgets/WidgetContainerElement/WidgetContainerElement.js
+++ src/model/widgets/WidgetContainerElement/WidgetContainerElement.js
@@ -10,7 +10,7 @@ export default class WidgetContainerElement extends WidgetElement {
         const label = this.preview();
         const canAdd = this.type == 'specific-container' || this.type == 'specific-element-container';
         const canEdit = this.constructor.toolbar.edit;
-        const canCopy = this.type !== 'specific-element';
+        const canCopy = this.type !== 'specific-element' && this.type !== 'element-container';
         const children = this.data ? this.data.map(child => child.createElement()).join('') : "";
         return template({id: this.id, type: this.type, widget: this.widget, icon, label, canAdd, canEdit, canCopy, children});
     }

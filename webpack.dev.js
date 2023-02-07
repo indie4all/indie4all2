c395e5bca8f9e77eecfb38d9528a161c1442ddd3
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-02-07 14:19:44 +0100

    Analyze bundle size by components

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
  const { merge } = require('webpack-merge');
  const common = require('./webpack.common.js');
+ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const devConfig = {
   mode: 'development',
   watch: true,
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  plugins: [ new BundleAnalyzerPlugin({analyzerMode: 'static'}) ]
 };
 
 module.exports = common.map(config => merge(config, devConfig))

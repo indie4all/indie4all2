b5a725a55a7d69cc892642665bc63e6d42dc4286
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-05-25 08:34:21 +0200

    Information messages to measure server response time

--- controllers/modelController.ts
+++ controllers/modelController.ts
@@ -13,7 +13,10 @@ const VALID_COVER_PATTERN = /^data:([-\w.]+\/[-\w.+]+)?;base64,([A-Za-z0-9+/]{4}
 const VALID_COLOR_PATTERN = /^#[0-9a-fA-F]{6}$/;
 
 const copyAssets = async function (folder: string, color: string, cover: string, mode: string) {
+
+    logger.info("Copying unit assets into the destination directory");
     await fs.copy(config.get("folder.assets"), folder, { overwrite: true })
+    logger.info("Generating custom theme");
     // Generate the css of the current theme
     const themeTemplate = './templates/theme.scss';
     const realColor = VALID_COLOR_PATTERN.test(color) ? color : '#000000';
@@ -23,11 +26,15 @@ const copyAssets = async function (folder: string, color: string, cover: string,
         sass.render({ data, includePaths: [themeTemplate], outputStyle: 'compressed' }, (err, result) => { resolve(result.css) }));
     await fs.writeFile(folder + '/generator/content/v4-7-2/css/stylesCustom.min.css', css as string);
     // Remove scorm libraries if the unit is not of SCORM type
-    if (mode !== "SCORM")
+    if (mode !== "SCORM") {
+        logger.info("Removing scorm-related assets");
         await fs.rm(folder + '/scorm/', { recursive: true, force: true });
+    }
 }
 
 const generate = function (req: Request, res: Response, onGenerated: Function, mode = "Local") {
+
+    logger.info("Generating unit in " + mode + " mode");
     const model = req.body as any;
     if (Object.keys(model).length === 0 && Object.getPrototypeOf(model) === Object.prototype)
         return res.status(StatusCodes.NO_CONTENT).send();
@@ -41,8 +48,10 @@ const generate = function (req: Request, res: Response, onGenerated: Function, m
     const modelJSON = timestamp + "_" + config.get("file.model.json");
     const modelXText = timestamp + "_" + config.get("file.model.xtext");
     fs.writeFile(modelJSON, JSON.stringify(model), function (error) {
-        if (error)
+        if (error) {
+            logger.err(error);
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
 
         // Create a folder whose name is the current timestamp plus 10 minutes
         const outputFolder = `${config.get("folder.previews")}/${timestamp}`;
@@ -61,6 +70,7 @@ const generate = function (req: Request, res: Response, onGenerated: Function, m
 
 const onPublishUnit = async function (res: Response, output: string, folder: string, model: any) {
     await copyAssets(`${folder}/${config.get("folder.assets")}`, model.color, model.cover, model.mode);
+    logger.info("Zipping the generated unit folder");
     const zip = new AdmZip();
     zip.addLocalFolder(folder);
     const binary = zip.toBuffer();

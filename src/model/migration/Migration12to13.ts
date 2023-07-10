36c0668edf3ebe77fee5357f2544ea0576d74e95
Pablo Muñoz <pablodavid.munoz@upct.es>
2023-07-10 12:42:41 +0200

    [Fix] Migration class name

--- src/model/migration/Migration12to13.ts
+++ src/model/migration/Migration12to13.ts
@@ -1,29 +1,28 @@
 import Utils from "../../Utils";
-import I18n from "../../I18n";
-import { WidgetCalloutParams} from "../../types";
+import { WidgetCalloutParams } from "../../types";
 
-export default class Migration11to12 {
+export default class Migration12to13 {
 
     static run(model: any) {
         const texts = Utils.findObjectsOfType(model, "TextBlock");
-        texts.filter(text => text.data?.style && text.data?.style != "default").forEach( text => {
+        texts.filter(text => text.data?.style && text.data?.style != "default").forEach(text => {
 
             const child = structuredClone(text);
             delete child.data.style;
 
-            const params : WidgetCalloutParams = {
+            const params: WidgetCalloutParams = {
                 text: "",
                 style: text.data.style,
                 colorTheme: "",
                 animation: "simple"
             }
-            
+
             text.id = Utils.generate_uuid();
             text.widget = "Callout";
             text.data = [child];
             text.params = params;
-            
+
         });
-        
+
     }
 }
